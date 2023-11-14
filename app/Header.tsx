704b1968190e2db6e4a9147ed48d11741710bcf6
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-14 13:56:12 +0600

    style the active link in the navbar

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,23 +1,30 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { HiOutlineBugAnt } from "react-icons/hi2";
+
 export default function Header() {
-  const links = [
-    { href: "/", label: "Dashboard" },
-    { href: "/issues", label: "Issues" },
-  ];
+  const currentPath = usePathname();
 
   return (
-    <header className="border-b mb-5">
-      <nav className="flex justify-between p-5 max-w-7xl mx-auto">
+    <header className="mb-5 border-b">
+      <nav className="mx-auto flex max-w-7xl justify-between p-5">
         <Link href="/">
-          <HiOutlineBugAnt />
+          <HiOutlineBugAnt className="text-xl" />
         </Link>
         <ul className="flex space-x-6">
           {links.map(({ href, label }) => (
             <li key={`${href}${label}`}>
               <Link
                 href={href}
-                className="text-zinc-500 hover:text-zinc-800 transition-colors">
+                className={classNames({
+                  "text-zinc-900": currentPath === href,
+                  "text-zinc-500": currentPath !== href,
+                  "transition-colors hover:text-zinc-800": true,
+                })}
+              >
                 {label}
               </Link>
             </li>
@@ -27,3 +34,8 @@ export default function Header() {
     </header>
   );
 }
+
+const links = [
+  { href: "/", label: "Dashboard" },
+  { href: "/issues", label: "Issues" },
+];
