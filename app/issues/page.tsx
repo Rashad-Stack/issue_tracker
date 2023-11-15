1b2948e888e284dc149daac49c704237f17334ea
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-15 19:00:21 +0600

    Refactor: Organize imports statements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,7 @@
+import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import Link from "../components/Link";
 import IssueAction from "./issueAction";
 
 export default async function IssuesPage() {
