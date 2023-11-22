f3030d035c6e21ec8dbc526f63713ab49808f7a0
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-22 11:48:25 +0600

    Add the dropdown menu to show the current user

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -15,7 +22,7 @@ export default function Header() {
     <header className="mb-5 border-b">
       <nav className="p-5">
         <Container>
-          <Flex>
+          <Flex align="center" justify="between">
             <Flex align="center" gap="3">
               <Link href="/">
                 <HiOutlineBugAnt className="text-xl" />
@@ -40,19 +47,26 @@ export default function Header() {
 
             <Box>
               {status === "authenticated" ? (
-                <Link
-                  href="/api/auth/signout"
-                  className="text-zinc-500 transition-colors hover:text-zinc-800"
-                >
-                  Sign out
-                </Link>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger className="cursor-pointer">
+                    <Avatar
+                      src={session.user!.image!}
+                      fallback="?"
+                      size="2"
+                      radius="full"
+                    />
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                      <Text size="2">{session.user?.name}</Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                      <Link href="/api/auth/signout">Sign out</Link>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Root>
               ) : (
-                <Link
-                  href="/api/auth/signin"
-                  className="text-zinc-500 transition-colors hover:text-zinc-800"
-                >
-                  Sign in
-                </Link>
+                <Link href="/api/auth/signin">Sign in</Link>
               )}
             </Box>
           </Flex>
