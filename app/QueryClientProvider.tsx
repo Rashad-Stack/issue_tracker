67446ed6048ee4f73aeb41c194e7396d1e7d7f6a
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-11-22 17:19:37 +0600

    Add react query client

---
+++ app/QueryClientProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { PropsWithChildren } from "react";
+import {
+  QueryClient,
+  QueryClientProvider as ReactQueryClientProvider,
+} from "@tanstack/react-query";
+
+export default function QueryClientProvider({ children }: PropsWithChildren) {
+  const queryClient = new QueryClient();
+
+  return (
+    <ReactQueryClientProvider client={queryClient}>
+      {children}
+    </ReactQueryClientProvider>
+  );
+}
